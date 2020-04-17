a86a875292ffc97d062c3f796b2dd08b105f7b93
LandryNorris <landry.norris@live.com>
2020-04-16 19:23:16 -0500

    Added Copyright messages to source files.

--- src/SaveManager.java
+++ src/SaveManager.java
@@ -1,3 +1,18 @@
+/*
+ Copyright (c) 2020 Landry Norris. This file is part of
+ Digital Circuit Simulator, an open source simulator for digital circuits.
+ Digital Circuit Simulator is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ Digital Circuit Simulator is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with the Digital Circuit Simulator Library.  If not, see http://www.gnu.org/licenses.
+ */
+
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
